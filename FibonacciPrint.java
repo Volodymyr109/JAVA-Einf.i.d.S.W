6caf1e00035c6fdfc807c790e433775fce490942
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-04-13 18:02:25 +0200

    Update FibonacciPrint.java

--- FibonacciPrint.java
+++ FibonacciPrint.java
@@ -1,16 +1,19 @@
 public class FibonacciPrint {
     public static void main(String[] args) {
 
-        int n = 10;
+        int n = 10; //menge der fib. zahlen
+
+        // new macht speicherplatz für "neue" klasse frei, push quasi
         Fibonacci fibonacci = new Fibonacci();
-        System.out.println("+---+--------+");
-        System.out.println("| n | f(n)   |");
-        System.out.println("+---+--------+");
-        System.out.printf("|   |        |\n", 0, 0);
+        System.out.println("+---+--------+"); //struktur outputs
+        System.out.println("| n | f(n)   |"); //struktur outputs
+        System.out.println("+---+--------+"); //struktur outputs
+        System.out.printf("|   |        |\n", 0, 0); //struktur outputs
+        // schleife geht n mal, und importiert klasse fibonacci, die die fib. zahlen rechnet
         for (int i = 1; i <= n; i++) {
             int fn = fibonacci.next();
-            System.out.printf("|%3d|%8d|\n", i, fn);
+            System.out.printf("|%3d|%8d|\n", i, fn); //struktur outputs, %3d ist als quasi "tostring" benötigt
         }
-        System.out.println("+---+--------+");
+        System.out.println("+---+--------+"); //struktur outputs
     }
 }
