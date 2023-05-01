cda34fad46682c683d9cab35e86768cc3d42b149
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-05-01 18:35:17 +0200

    Update ArenaTest.java

--- ArenaTest.java
+++ ArenaTest.java
@@ -1,26 +1,38 @@
 /*
-Eingaben im Bereich von 0 bis 30 Grad: (0, 1.2), (0.5, 1.3), (1.5, 1.3), (Math.cos(Math.toRadians(15)), Math.sin(Math.toRadians(15)))
-Eingaben im Bereich von 30 bis 60 Grad: (1.2, 0), (1.3, 0.5), (1.3, 1.5), (Math.cos(Math.toRadians(45)), Math.sin(Math.toRadians(45)))
-Eingaben im Bereich von 60 bis 90 Grad: (0, -1.2), (-0.5, -1.3), (-1.5, -1.3), (Math.cos(Math.toRadians(75)), Math.sin(Math.toRadians(75)))
-Eingaben im Bereich von 90 bis 120 Grad: (-1.2, 0), (-1.3, -0.5), (-1.3, -1.5), (Math.cos(Math.toRadians(105)), Math.sin(Math.toRadians(105)))
-Eingaben im Bereich von 120 bis 150 Grad: (0, 1.2), (0.5, -1.3), (1.5, -1.3), (Math.cos(Math.toRadians(135)), Math.sin(Math
+Eingaben im Bereich {0, 1.2}, {0.5, 1.3}, {1.5, 1.3}, {Math.cos(Math.toRadians(15)), Math.sin(Math.toRadians(15))}, //1
+                    {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(45)), Math.sin(Math.toRadians(45))}, //2
+                    {0, -1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(75)), Math.sin(Math.toRadians(75))}, //3
+                    {-1.2, 0}, {-1.3, -0.5}, {-1.3, -1.5}, {Math.cos(Math.toRadians(105)), Math.sin(Math.toRadians(105))}, //4
+                    {0, 1.2}, {0.5, -1.3}, {1.5, -1.3}, {Math.cos(Math.toRadians(135)), Math.sin(Math.toRadians(135))}, //5
+                    {-1.2, 0}, {-1.3, -0.5}, {-1.3, -1.5}, {Math.cos(Math.toRadians(175)), Math.sin(Math.toRadians(175))}, //6
+                    {0, 1.2}, {0.5, -1.3}, {1.5, -1.3}, {Math.cos(Math.toRadians(205)), Math.sin(Math.toRadians(205))}, //7
+                    {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(235)), Math.sin(Math.toRadians(235))}, //8
+                    {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(265)), Math.sin(Math.toRadians(265))}, //9
+                    {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(295)), Math.sin(Math.toRadians(295))}, //10
+                    {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(315)), Math.sin(Math.toRadians(315))}, //11
+                    {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(345)), Math.sin(Math.toRadians(345))}, //12
  */
 
 public class ArenaTest {
     public static void main(String[] args) {
         double[][] tributes = {
-                {0, 1.2}, {0.5, 1.3}, {1.5, 1.3}, {Math.cos(Math.toRadians(15)), Math.sin(Math.toRadians(15))},
-                {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(45)), Math.sin(Math.toRadians(45))},
-                {0, -1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(75)), Math.sin(Math.toRadians(75))},
-                {-1.2, 0}, {-1.3, -0.5}, {-1.3, -1.5}, {Math.cos(Math.toRadians(105)), Math.sin(Math.toRadians(105))},
-                {0, 1.2}, {0.5, -1.3}, {1.5, -1.3}, {Math.cos(Math.toRadians(135)), Math.sin(Math.toRadians(135))},
-                {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(315)), Math.sin(Math.toRadians(315))},
-                {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(345)), Math.sin(Math.toRadians(345))}
+                {0, 1.2}, {0.5, 1.3}, {1.5, 1.3}, {Math.cos(Math.toRadians(15)), Math.sin(Math.toRadians(15))}, //1
+                {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(45)), Math.sin(Math.toRadians(45))}, //2
+                {0, -1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(75)), Math.sin(Math.toRadians(75))}, //3
+                {-1.2, 0}, {-1.3, -0.5}, {-1.3, -1.5}, {Math.cos(Math.toRadians(105)), Math.sin(Math.toRadians(105))}, //4
+                {0, 1.2}, {0.5, -1.3}, {1.5, -1.3}, {Math.cos(Math.toRadians(135)), Math.sin(Math.toRadians(135))}, //5
+                {-1.2, 0}, {-1.3, -0.5}, {-1.3, -1.5}, {Math.cos(Math.toRadians(175)), Math.sin(Math.toRadians(175))}, //6
+                {0, 1.2}, {0.5, -1.3}, {1.5, -1.3}, {Math.cos(Math.toRadians(205)), Math.sin(Math.toRadians(205))}, //7
+                {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(235)), Math.sin(Math.toRadians(235))}, //8
+                {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(265)), Math.sin(Math.toRadians(265))}, //9
+                {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(295)), Math.sin(Math.toRadians(295))}, //10
+                {1.2, 0}, {1.3, 0.5}, {1.3, 1.5}, {Math.cos(Math.toRadians(315)), Math.sin(Math.toRadians(315))}, //11
+                {0, 1.2}, {-0.5, -1.3}, {-1.5, -1.3}, {Math.cos(Math.toRadians(345)), Math.sin(Math.toRadians(345))}, //12
         };
 
         for (double[] tribute : tributes) {
             int area = Arena.getArea(tribute[0], tribute[1]);
-            System.out.println("(" + tribute[0] + ", " + tribute[1] + ") is in area " + area);
+            System.out.println("( x: " + tribute[0] + ", y: " + tribute[1] + ") nicht aktiv in Area: " + area);
         }
     }
 }
